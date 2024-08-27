6c466be6833500b2cf5150827b5fe91be99ddc0f
Elizabeth131-max <adesanyaelizabeth29@gmail.com>
2024-08-27 01:52:24 -1100

    added another function

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+function myWeather(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#topic");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchInput(city) {
+  let apiKey = "3f7ef2457d28667co20b059a40t047b0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(myWeather);
+}
+
 function searchMe(event) {
   event.preventDefault();
   let enterInput = document.querySelector("#enter-input");
-  let cityElement = document.querySelector("#topic");
-  cityElement.innerHTML = enterInput.value;
+
+  searchInput(enterInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
