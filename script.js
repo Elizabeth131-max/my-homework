c6967f0dae585214b5e45afa269b5d249374df7f
Elizabeth131-max <adesanyaelizabeth29@gmail.com>
2024-08-27 19:36:53 -1100

    updated my weather icon and styled it in css

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function myWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = ` <img src="${response.data.condition.icon_url}"class="icon" />`;
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
